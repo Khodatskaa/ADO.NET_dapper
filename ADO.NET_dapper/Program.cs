11246af7f56cd838fb83a14d0c9cd0163c2dd78e
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-02 14:38:03 +0300

    Ex 2 was done

--- ADO.NET_dapper/Program.cs
+++ ADO.NET_dapper/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace ADO.NET_dapper
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ADO.NET_dapper
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string connectionString = "Server=localhost;Database=mailinglistdb;User ID=root;Password=yourpassword;"; 
+
+            using (IDbConnection db = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    db.Open();
+                    Console.WriteLine("Successfully connected to the database.");
+                    db.Close();
+                    Console.WriteLine("Successfully disconnected from the database.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error connecting to the database: " + ex.Message);
+                }
+            }
         }
     }
 }
